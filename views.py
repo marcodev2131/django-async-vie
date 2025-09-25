ee705b0efe27aae9b904b593a98c1e6a68bec405
marcodev2131 <145700074+marcodev2131@users.noreply.github.com>
2025-09-24 23:54:32 -0300

    feat: ajustar view assíncrona

--- views.py
+++ views.py
@@ -1,9 +1,11 @@
 import time
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
 def api(request):
 	time.sleep(1)
 	payload = {"message": "Hello from Crowdbotics!"}
+	
 	if "task_id" in request.GET:
 		payload["task_id"] = request.GET["task_id"]
 	return JsonResponse(payload)
